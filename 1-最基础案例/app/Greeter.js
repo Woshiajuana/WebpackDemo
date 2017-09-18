d863008480941ee6dcb9e1136dac08ca966fa9ea
Woshiajuana <zhigang.chen@owulia.com>
2017-09-18 14:55:52 +0800

    更新

---
+++ 1-最基础案例/app/Greeter.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Administrator on 2017/9/18.
+ */
+import React, {Component} from 'react'
+import config from './config.json';
+
+class Greeter extends Component{
+    render() {
+        return (
+            <div>
+                {config.greetText}
+            </div>
+        );
+    }
+}
+
+export default Greeter
